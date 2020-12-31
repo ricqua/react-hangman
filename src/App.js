ba5a90fd843e4dc2ea9f1c534e6fb09af54a1d99
Ricqua <ricqua@gmail.com>
2020-12-31 12:22:49 +0900

    split the project into a rock papre scissors game only

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
-import Hangman from "./components/Hangman/Hangman";
 import RockPaperScissors from "./components/RockPaperScissors/RockPaperScissors";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <React.Fragment>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/hangman" component={Hangman} />
-          <Route path="/rockpaperscissors" component={RockPaperScissors} />
-        </Switch>
-      </React.Fragment>
-    </BrowserRouter>
+    <React.Fragment>
+      <RockPaperScissors />
+    </React.Fragment>
   );
 };
 

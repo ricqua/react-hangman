0343b7a612668fddb6ae19446b017eed5cb7df2d
Ricqua <ricqua@gmail.com>
2021-01-01 17:36:05 +0900

    All logic is working.  CSS is needing lots of work still

--- src/components/RockPaperScissors/RockPaperScissors.js
+++ src/components/RockPaperScissors/RockPaperScissors.js
@@ -1,52 +1,82 @@
 import React, { useState, useEffect } from "react";
 import "./rockPaperScissors.css";
-
-function computerAttack() {
-  const x = Math.floor(Math.random() * 3);
-  // console.log(x);
-  return x;
-}
+import { images } from "./images/index.js";
 
 function RockPaperScissors() {
   const [options, setOptions] = useState(["rock", "paper", "scissors"]);
-  const [computer, setComputer] = useState(options[computerAttack()]);
-  const [player, setPlayer] = useState("");
+  const [computer, setComputer] = useState("---");
+  const [player, setPlayer] = useState("---");
   const [victoryStatus, setVictoryStatus] = useState("");
+  const [score, setScore] = useState({ computerScore: 0, playerScore: 0 });
 
   function handleAttack(e) {
+    setComputer(options[Math.floor(Math.random() * 3)]);
     setPlayer(e.target.id);
+  }
 
-    if (player === computer) {
+  useEffect(() => {
+    if (player === "") {
+      setVictoryStatus("");
+    } else if (player === computer) {
       setVictoryStatus("DRAW");
-      console.log(victoryStatus);
     } else if (
       (player === options[1] && computer === options[0]) ||
       (player === options[2] && computer === options[1]) ||
       (player === options[0] && computer === options[2])
     ) {
       setVictoryStatus("WINNER");
-      console.log(victoryStatus);
+      setScore({ ...score, playerScore: score.playerScore + 1 });
     } else {
       setVictoryStatus("LOSER");
-      console.log(victoryStatus);
+      setScore({ ...score, computerScore: score.computerScore + 1 });
     }
-  }
+  }, [player, computer]);
 
   return (
     <React.Fragment>
-      <h1> Rock Paper Scissors game</h1>
-      <button onClick={handleAttack} id={options[0]}>
-        {options[0]}
-      </button>
-      <button onClick={handleAttack} id={options[1]}>
-        {options[1]}
-      </button>
-      <button onClick={handleAttack} id={options[2]}>
-        {options[2]}
-      </button>
-      <h1>
-        Player 1 chose {player}. Computer chose {computer}. {victoryStatus}
-      </h1>
+      <div className="wrapper">
+        <div className="main">
+          <header className="headerComponent">
+            <h1> Rock Paper Scissors</h1>
+          </header>
+          <div className="attackButtons">
+            <img
+              onClick={handleAttack}
+              id={options[0]}
+              src={images.images[0]}
+              alt="rock"
+              className="attackButton"
+            />
+            <img
+              onClick={handleAttack}
+              id={options[1]}
+              src={images.images[1]}
+              alt="paper"
+              className="attackButton"
+            />
+            <img
+              onClick={handleAttack}
+              id={options[2]}
+              src={images.images[2]}
+              alt="scissors"
+              className="attackButton"
+            />
+          </div>
+          <section className="victoryArea">{victoryStatus}</section>
+          <div className="resultsArea">
+            <div className="playerSide">
+              <h1 className="choice">{player}</h1>
+              <h1 className="player">Player:</h1>
+              <h1 className="score">{score.playerScore}</h1>
+            </div>
+            <div className="playerSide">
+              <h1 className="choice">{computer}</h1>
+              <h1 className="player">Computer:</h1>
+              <h1 className="score">{score.computerScore}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
---
+++ src/components/RockPaperScissors/images/index.js
@@ -0,0 +1,7 @@
+import rock from "./rock.png";
+import paper from "./paper.png";
+import scissors from "./scissors.png";
+
+export const images = {
+  images: [rock, paper, scissors],
+};

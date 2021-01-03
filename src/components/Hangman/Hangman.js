eafd5b2d5463d8e79079d5435c70960717f32173
Ricqua <ricqua@gmail.com>
2021-01-03 20:40:52 +0900

    quick commit

--- src/components/Hangman/Hangman.js
+++ src/components/Hangman/Hangman.js
@@ -3,32 +3,45 @@ import "./hangman.css";
 import HeaderArea from "./Header";
 import WhiteboardArea from "./WhiteboardArea";
 import GuessedLetters from "./GuessedLetters";
-import ResultArea from "./ResultArea";
+// import ResultArea from "./ResultArea";
 import { words3 } from "./data";
 
 const Hangman = () => {
-  let [isLives, setLives] = useState(0);
-  let [isGuessedLetters, setGuessedLetters] = useState("");
-  let [isAnswer, setAnswer] = useState("");
-  // let [isDisplayWord, setDisplayWord] = useState("");
-
-  useEffect(() => {
-    setAnswer(
-      words3.wordlist[Math.floor(Math.random() * words3.wordlist.length - 1)]
-    );
-    // setAnswer("cats");
-    // setDisplayWord(isAnswer);
-  }, []);
+  const [isLives, setLives] = useState(0);
+  const [isGuessedLetters, setGuessedLetters] = useState("");
+  const [isAnswer, setAnswer] = useState(
+    words3.wordlist[Math.floor(Math.random() * words3.wordlist.length - 1)]
+  );
+  const [isDisplayWord, setDisplayWord] = useState(
+    isAnswer.split("").map(() => {
+      return " _ ";
+    })
+  );
 
   const handleGuess = (e) => {
     e.preventDefault();
     setGuessedLetters(isGuessedLetters.concat(e.target.id));
     e.target.disabled = true;
-
     if (isAnswer.indexOf(e.target.id) < 0) {
       setLives(isLives + 1);
     }
   };
+
+  useEffect(() => {
+    const lastLetter = isGuessedLetters[isGuessedLetters.length - 1];
+    const letterIndex = isAnswer.indexOf(lastLetter);
+    console.log(isAnswer);
+    console.log(isDisplayWord.splice(letterIndex, 10, "help"));
+
+    if (letterIndex >= 0) {
+      // setDisplayWord((prevDisplayWord) => {
+      //   return [...prevDisplayWord, (isDisplayWord[letterIndex], lastLetter)];
+
+      //   // [(isDisplayWord[letterIndex] = lastLetter)];
+      // });
+      setDisplayWord(isDisplayWord.splice(letterIndex, 1, lastLetter));
+    }
+  }, [isGuessedLetters]);
 
   const keyboardLetters = (
     <React.Fragment>
@@ -62,7 +75,8 @@ const Hangman = () => {
           </div>
           <GuessedLetters isGuessedLetters={isGuessedLetters} />
         </div>
-        <ResultArea targetWord={isAnswer} guessedLetters={isGuessedLetters} />
+        {/* <ResultArea targetWord={isAnswer} guessedLetters={isGuessedLetters} /> */}
+        <div className="wordboardArea">{isDisplayWord}</div>
       </div>
     </React.Fragment>
   );

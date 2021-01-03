59c6de525a04e4afd6c14bfd0ebf9e5511b41d21
Ricqua <ricqua@gmail.com>
2021-01-03 21:02:47 +0900

    Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-// import RockPaperScissors from "./components/RockPaperScissors/RockPaperScissors";
-import Hangman from "./components/Hangman/Hangman";
+import RockPaperScissors from "./components/RockPaperScissors/RockPaperScissors";
+// import Hangman from "./components/Hangman/Hangman";
 
 const App = () => {
   return (
     <React.Fragment>
-      {/* <RockPaperScissors /> */}
-      <Hangman />
+      <RockPaperScissors />
+      {/* <Hangman /> */}
     </React.Fragment>
   );
 };
